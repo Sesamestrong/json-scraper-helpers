4a708de17d7e9fca66a45134287148c11918170b
sesamestrong <doolie.healey@gmail.com>
2020-01-13 04:02:03 -0500

    First draft

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const app = express();
+
+const bodyParser = require("body-parser");
+
+const moment = require("moment");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post("/matchDate", (req, res) => {
+    const {
+        year,
+        month,
+        day,
+        format,
+        patterns
+    } = req.body;
+    const form = moment(`${year}/${month}/${day}`).format(format);
+    let ret = null;
+    for (let pattern in patterns) {
+        const rx = new RegExp(pattern);
+        if (form.match(rx)) {
+            ret = form.replace(rx, patterns[pattern]);
+            break;
+        }
+    }
+    res.text(ret);
+});
+
+app.post("/btoa", (req, res) => res.text(btoa(req.body.src)));
+app.post("/atob", (req, res) => res.text(atob(req.body.src)));
+
+app.post("/match", (req, res) => {
+    const {str,pattern}=req.body;
+    const match=str.match(pattern);
+    
+    res.json(match&&{
+        match:match[0],
+        ...match.slice(1)
+    });
+});
+
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>console.log("Listening on port",PORT));
